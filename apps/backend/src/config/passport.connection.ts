33d3e039f37c4cc6971434bb21e84da9e8e93eab
ChaitanyaRankhamb54 <chaitanyarankhamb007@gmail.com>
2026-09-22 13:59:47 +0530

    change the callback url

--- apps/backend/src/config/passport.connection.ts
+++ apps/backend/src/config/passport.connection.ts
@@ -18,7 +18,7 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID || "",
       clientSecret: process.env.GOOGLE_CLIENT_SECRET || "",
-      callbackURL: "http://localhost:7000/auth/google/callback",
+      callbackURL: `http://localhost:${process.env.PORT || 6001}/auth/google/callback`,
     },
     async (_accessToken, _refreshToken, profile, cb) => {
       try {
